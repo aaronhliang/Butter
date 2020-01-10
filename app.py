19473cd0be04fa608651347deab69ed413ace825
Sumin Kim <SuminKim@Sumins-MacBook-Air.local>
2020-01-09 19:33:03 -0500

    Added db connection and coupon details page

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+import json
+from pymongo import MongoClient
+from bson import ObjectId
+
 app = Flask(__name__)
+client = MongoClient("mongodb+srv://suminkim:miknimus@honey-irl-ywt1c.mongodb.net/test?retryWrites=true&w=majority")
+honey_db = client['honey_db']
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route("/coupons/<coupon_id>", methods=["POST"])
+def favoriteCoupon(coupon_id):
+    return coupon_id
+
