32fb99f7fe3e5045685e027293426f24f62d1b48
Sumin Kim <SuminKim@Sumins-MacBook-Air.local>
2020-01-09 22:27:20 -0500

    Added db functions

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import requests
 
 app = Flask(__name__)
 
-# client = MongoClient("mongodb+srv://suminkim:miknimus@honey-irl-ywt1c.mongodb.net/test?retryWrites=true&w=majority")
-# honey_db = client['honey_db']
+client = MongoClient("mongodb+srv://suminkim:miknimus@honey-irl-ywt1c.mongodb.net/test?retryWrites=true&w=majority")
+honey_db = client['honey_db']
 
 api_key = 'MBjxxSsO'
 
@@ -20,22 +20,33 @@ def start():
 def index():
     return render_template("index.html")
 
-@app.route("/coupons/<coupon_id>", methods=["POST", "GET"])
+@app.route("/coupons/<coupon_id>", methods=["POST", "GET", "DELETE"])
 def couponSpecific(coupon_id):
     # tbd
-    if(request.method=="POST"): 
-        return coupon_id
+    url = 'https://api.discountapi.com/v2/deals/' + str(coupon_id)
+    result = requests.get(url).json()['deal']
+    saved_coupons_collection = honey_db['savedCoupons']
+
+    if(request.method=="DELETE"):
+        delete_item = int(coupon_id)
+        saved_coupons_collection.delete_one({"id": delete_item})
+        return "Coupon deleted"
+    elif(request.method=="POST"):
+        saved_coupons_collection.insert_one(result)
+        api_result = requests.get(url).json()['deal']
+        return "Coupon added"
     else:
         url = 'https://api.discountapi.com/v2/deals/' + str(coupon_id)
-        result = requests.get(url).json()
+        result = requests.get(url).json()['deal']
         return render_template("coupon_details.html", coupon=result)
 
 @app.route('/coupons', methods=['GET'])
 def coupons():
     results = get_coupons(22101)
     populate_map(results)
-
+    
     return render_template("coupons.html", coupons=results)
+        
 
 
 def get_coupons(zip_code, radius=10):
