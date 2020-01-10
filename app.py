26dc94971606a89817f242e00a799516809d58ff
jeffersonxu <me@jeffersonxu.com>
2020-01-09 20:41:07 -0500

    geotag info

--- app.py
+++ app.py
@@ -13,9 +13,24 @@ def hello_world():
 @app.route('/coupons', methods=['GET'])
 def coupons():
     results = get_coupons(22101)
+    populate_map(results)
+
     return render_template("coupons.html", coupons=results)
 
 
-def get_coupons(zip_code):
-    url = 'https://api.discountapi.com/v2/deals?location=' + str(zip_code) + '&api_key=' + api_key
+def get_coupons(zip_code, radius=10):
+    url = 'https://api.discountapi.com/v2/deals?location=' + str(zip_code) + '&radius=' + str(radius) + '&api_key=' + api_key
+
     return requests.get(url).json()['deals']
+
+
+def populate_map(coupons):
+    for coupon in coupons:
+        lat = coupon['deal']['merchant']['latitude']
+        lon = coupon['deal']['merchant']['longitude']
+
+        # play around with ur map here
+
+        print("lat:", lat)
+        print("longitude: ", lon)
+        print()
