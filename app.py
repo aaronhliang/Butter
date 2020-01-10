a6999dc405efcd9cc2943b69851b799b256b2b69
jeffersonxu <me@jeffersonxu.com>
2020-01-09 19:27:18 -0500

    api call basic functionality

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
 app = Flask(__name__)
+
+api_key = 'MBjxxSsO'
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/coupons', methods=['GET'])
+def coupons():
+    results = get_coupons(22101)
+    return render_template("coupons.html", coupons=results)
+
+
+def get_coupons(zip_code):
+    url = 'https://api.discountapi.com/v2/deals?location=' + str(zip_code) + '&api_key=' + api_key
+    return requests.get(url).json()['deals']
