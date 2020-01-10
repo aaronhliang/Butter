b6f30930dfab40cb738e4e9c8642e24cc7512b50
Lu Cao <lu.cao@vanderbilt.edu>
2020-01-09 19:36:50 -0500

    test

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/map')
+def map():
+    return render_template('Map.html')
