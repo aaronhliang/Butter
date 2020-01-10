da8e57443481ef06ef52c1bb93c471c78d85cb32
Sumin Kim <SuminKim@Sumins-MacBook-Air.local>
2020-01-09 20:55:56 -0500

    Added requirements, specific details about a coupon route

--- app.py
+++ app.py
@@ -6,19 +6,24 @@ import requests
 
 app = Flask(__name__)
 
-client = MongoClient("mongodb+srv://suminkim:miknimus@honey-irl-ywt1c.mongodb.net/test?retryWrites=true&w=majority")
-honey_db = client['honey_db']
+# client = MongoClient("mongodb+srv://suminkim:miknimus@honey-irl-ywt1c.mongodb.net/test?retryWrites=true&w=majority")
+# honey_db = client['honey_db']
 
 api_key = 'MBjxxSsO'
-
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route("/coupons/<coupon_id>", methods=["POST"])
-def favoriteCoupon(coupon_id):
-    return coupon_id
+@app.route("/coupons/<coupon_id>", methods=["POST", "GET"])
+def couponSpecific(coupon_id):
+    # tbd
+    if(request.method=="POST"): 
+        return coupon_id
+    else:
+        url = 'https://api.discountapi.com/v2/deals/' + str(coupon_id)
+        result = requests.get(url).json()
+        return render_template("coupon_details.html", coupon=result)
 
 @app.route('/coupons', methods=['GET'])
 def coupons():
