5cbb54d14e6e48180f57b9354441d734d3c3a00b
jeffersonxu <me@jeffersonxu.com>
2020-01-10 00:52:30 -0500

    added links to cards :)

--- app.py
+++ app.py
@@ -19,15 +19,19 @@ app.config['GOOGLEMAPS_KEY'] = get_my_key()
 # Initialize the extension
 GoogleMaps(app)
 
+
 @app.route('/')
 def start():
     return render_template("index.html")
 
+
 api_key = "MBjxxSsO"
+
 
 @app.route('/index')
 def index():
     return render_template("index.html")
+
 
 @app.route("/coupons/<coupon_id>", methods=["POST", "GET", "DELETE"])
 def couponSpecific(coupon_id):
@@ -36,11 +40,11 @@ def couponSpecific(coupon_id):
     result = requests.get(url).json()['deal']
     saved_coupons_collection = honey_db['savedCoupons']
 
-    if(request.method=="DELETE"):
+    if(request.method == "DELETE"):
         delete_item = int(coupon_id)
         saved_coupons_collection.delete_one({"id": delete_item})
         return "Coupon deleted"
-    elif(request.method=="POST"):
+    elif(request.method == "POST"):
         saved_coupons_collection.insert_one(result)
         api_result = requests.get(url).json()['deal']
         return "Coupon added"
@@ -49,17 +53,19 @@ def couponSpecific(coupon_id):
         result = requests.get(url).json()['deal']
         return render_template("coupon_details.html", coupon=result)
 
+
 @app.route('/coupons', methods=['GET'])
 def coupons():
     results = get_coupons(22101)
     populate_map(results)
-    
-    return render_template("coupons.html", coupons=results)
-        
 
-def get_coupons(zip_code, radius = 10):
+    return render_template("coupons.html", coupons=results)
+
+
+def get_coupons(zip_code, radius=10):
     url = 'https://api.discountapi.com/v2/deals?location=' + str(zip_code) + '&radius=' + str(radius) + '&api_key=' + api_key
     return requests.get(url).json()['deals']
+
 
 @app.route("/map")
 def mapview():
@@ -74,13 +80,15 @@ def mapview():
         lng=-76.995700,
         markers=markers,
         style="height:500px;width:100%;margin:0;",
-        zoom = 18
+        zoom=18
     )
     return render_template('Map.html', sndmap=sndmap)
+
 
 def populate_map(coupons):
     markers = []
     for coupon in coupons:
+        couponId = str(coupon['deal']['id'])
         lat = coupon['deal']['merchant']['latitude']
         lon = coupon['deal']['merchant']['longitude']
         title = coupon['deal']['short_title']
@@ -90,7 +98,7 @@ def populate_map(coupons):
                 'lat': lat,
                 'lng': lon,
                 'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-                'infobox': "<div><h3>" + title + " </h3><p>" + terms + "</div>"
+                'infobox': "<div><h3><a href=/coupons#" + couponId + ">" + title + "</a></h3><p>" + terms + "</div>"
             }
         )
 
